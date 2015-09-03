e0e54f482a3a824d216a90c4b0aba6471828bf01
astur <astur@yandex.ru>
2015-09-04 01:00:03 +0300

    Sync test version

--- index.js
+++ index.js
@@ -1,3 +1,101 @@
-module.exports = function(){
-    return;
-};
+var fs = require('fs');
+
+function MRX(){
+    if(!(this instanceof MRX)) {return new MRX();}
+    var $ = [];
+    var self = this;
+    var _type = function(o) {
+        return Object.prototype.toString.call(o).split(' ')[1].slice(0,-1);
+    };
+
+    this.clear = function(){
+        $ = [];
+    };
+
+    this.count = function(){
+        return $.length;
+    };
+
+    this.add = function(a){
+        if (arguments.length === 0) {
+            throw new Error('Nothing to add');
+        }
+        if (arguments.length > 1) {
+            [].slice.call(arguments).forEach(function(a) {
+                self.add(a);
+            });
+            return;
+        }
+        if (_type(a) === 'Array') {
+            a.forEach(function(a) {
+                self.add(a);
+            });
+            return;
+        }
+        if (_type(a) === 'Object') {
+            if (a.type || a.type === 'folder') {
+                self.add(a.children);
+            } else if (a.url) {
+                self.add(a.url);
+            } else if (a.href) {
+                self.add(a.href);
+            } else {
+                throw new Error('Can add this');
+            }
+            return;
+        }
+        if (_type(a) !== 'String') {
+            throw new Error('Can add this');
+        }
+        //url correctoins
+        //other magic
+        $.push(a);
+    };
+
+    this.get = function(i){
+        return $[i];
+    };
+
+    this.last = function(){
+        return $[this.count()-1];
+    };
+
+    this.find = function(q){
+        // there will be magic
+        // String, Object, RegEx or (mb) Number
+        return $.filter(function(v) {return v.indexOf(q) !== -1;});
+    };
+
+    this.load = function(filename, enc){
+        enc = enc || 'utf-8';
+
+        var mre = /<a\s[^>]*(href\s*=\s*(\")?htt)[^>]*>/gi;
+        var rre = /<a.*?href\s*=\s*(\")?(http[^"]+?)\1\s[^>]*>/ig;
+
+        var src = fs.readFileSync(filename, enc);
+
+        try {
+            src = JSON.parse(src);
+        } catch (e) {
+            if (mre.test(src)) {
+                src = src.match(mre);
+                for(var i = 0, l = src.length; i<l; i++) {
+                    src[i] = src[i].replace(rre, '$2');
+                    src[i] = src[i].replace('&amp;', '&');
+                }
+            } else {
+                src = src.split(/\s*\n\s*/);
+            }
+        }
+        this.add(src);
+    };
+
+    this.save = function(filename){
+        fs.writeFileSync(filename, $.join('\n'));
+        //save to file (json, html, ...)
+        //.bak, ~file or kind of...
+    };
+
+}
+
+module.exports = MRX;
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,56 @@
 var tape = require('tape');
-var mrx = require('../');
+var MRX = require('../');
+var mrx = MRX();
+
+tape.test.skip('interface', function (t) {
+
+    t.ok(typeof MRX === 'function', 'MRX is Function');
+    t.ok(typeof mrx === 'object', 'mrx is Object');
+
+    t.ok(typeof mrx.clear === 'function', 'mrx has method "clear"');
+    t.ok(typeof mrx.count === 'function', 'mrx has method "count"');
+    t.ok(typeof mrx.add === 'function', 'mrx has method "add"');
+    t.ok(typeof mrx.get === 'function', 'mrx has method "get"');
+    t.ok(typeof mrx.last === 'function', 'mrx has method "last"');
+    t.ok(typeof mrx.find === 'function', 'mrx has method "find"');
+    t.ok(typeof mrx.load === 'function', 'mrx has method "load"');
+    t.ok(typeof mrx.save === 'function', 'mrx has method "save"');
+
+    t.end();
+});
 
 tape.test('base', function (t) {
 
-    t.ok(typeof mrx === 'function', 'mrx is Function');
+    t.same(mrx.count(), 0, 'new mrx is empty');
+    t.throws(function(){mrx.add();},
+        Error,
+        'needs something to add');
+    t.throws(function(){mrx.add(1);},
+        Error,
+        'adds only strings');
+
+    mrx.add('http://yandex.ru/');
+    mrx.add('http://rambler.ru/');
+
+    t.same(mrx.count(), 2, 'count mrx items after 2 adds');
+    t.same(mrx.find('.ru').length, 2, 'find by substring');
+
+    mrx.add(['http://ferra.ru/', 'http://price.ru/'], 'http://ya.ru/');
+
+    t.same(mrx.count(), 5, 'count mrx items after group add');
+    t.same(mrx.last(), 'http://ya.ru/', 'get last item');
+    t.same(mrx.get(0), 'http://yandex.ru/', 'get first item');
+
+    mrx.save(__dirname + '/links.raw');
+
+    mrx.clear();
+    t.same(mrx.count(), 0, 'empty mrx');
+    
+    mrx.load(__dirname + '/links.raw');
+
+    t.same(mrx.count(), 5, 'loaded from file');
+
+    require('fs').unlinkSync(__dirname + '/links.raw');
 
     t.end();
 });
