9d831b9859e0c3e4ef6b81553293f0fea239cdbe
astur <astur@yandex.ru>
2015-09-07 18:03:34 +0300

    Async mrx passed tests

--- index.js
+++ index.js
@@ -3,87 +3,77 @@ function MRX(){
     if(!(this instanceof MRX)) {return new MRX();}
 
     var $ = [];
-    var self = this;
-    var _type = function(o) {
+    var _type = function(o){
         return Object.prototype.toString.call(o).split(' ')[1].slice(0,-1);
     };
-
-    this.clear = function(){
-        $ = [];
-    };
-
-    this.count = function(){
-        return $.length;
-    };
-
-    this.add = function(a){
-        if (arguments.length === 0) {
-            throw new Error('Nothing to add');
+    var pureUrl = function(url){
+        if(/\/\/[^/]+$/i.test(url)){
+            url = url + '/';
         }
-        if (arguments.length > 1) {
-            [].slice.call(arguments).forEach(function(a) {
-                self.add(a);
-            });
-            return;
+        return url;
+    };
+    var urlParse = function(url){
+        var re = /https?:\/\/(?:www\.)?([^/]+)\/([^?]+)?(\?[^#]*)?(#.*)?/i;
+        url = url.match(re);
+        if(url[2]) {
+            url[2] = url[2].replace('index.html', '');
+        }
+        return url;
+    };
+
+    this.clear = function(cb){
+        $ = [];
+        if(cb){cb(null);}
+    };
+
+    this.count = function(cb){
+        if(cb){cb(null, $.length);}
+    };
+
+    this.add = function(a, cb){
+        if (_type(a) === 'String') {
+            a = [a];
         }
         if (_type(a) === 'Array') {
             a.forEach(function(a) {
-                self.add(a);
+                if (_type(a) === 'String') {
+                    a = pureUrl(a);
+                    $.push(a);
+                } else {
+                    cb(new Error('Bad data'));
+                }
             });
-            return;
+            cb(null, a.length);
+        } else {
+            cb(new Error('Bad data'));
         }
-        if (_type(a) === 'Object') {
-            if (a.type && a.children) {
-                self.add(a.children);
-            } else if (a.url) {
-                self.add(a.url);
-            } else if (a.href) {
-                self.add(a.href);
-            } else {
-                throw new Error('Can\'t add this');
-            }
-            return;
-        }
-        if (_type(a) !== 'String') {
-            throw new Error('Can\'t add this');
-        }
-        //url correctoins
-        //other magic
-        $.push(a);
     };
 
-    this.remove = function(q){
+    this.remove = function(q, cb){
         var i = $.indexOf(q);
         if (i > -1) {
             $.splice(i, 1);
-            return true;
+            if(cb){cb(null, true);}
+        } else {
+            if(cb){cb(null, false);}
         }
-        return false;
     };
 
-    this.find = function(q){
+    this.find = function(q, cb){
         if (_type(q) === 'String') {
-            return $.indexOf(q) !== -1;
+            cb(null, $.indexOf(q) !== -1);
+        } else {
+            cb(new Error('Bad query'));
         }
-        if (_type(q) === 'RegExp') {
-            return $.filter(function(v) {return q.test(v);});
-        }
-        throw new Error('Bad query for find');
     };
 
-    this.check = function(q){
+    this.check = function(q, cb){
         if (_type(q) !== 'String' || !/http/i.test(q)) {
-            throw new Error('Bad URL for check');
-        }
-        function urlParse(x){
-            var re = /https?:\/\/(?:www\.)?([^/]+)\/([^?]+)?(\?[^#]*)?(#.*)?/i;
-            x = x.match(re);
-            if(x[2]) {
-                x[2] = x[2].replace('index.html', '');
-            }
-            return x;
+            cb(new Error('Bad URL for check'));
+            return;
         }
         var u = urlParse(q);
+        //console.log(q);////////
         var res = {
             same: $.indexOf(q) !== -1,
             similar: $.filter(function(v) {
@@ -117,7 +107,7 @@ function MRX(){
             res.domains[key] = $.filter(dCheck).length;
         }
 
-        return res;
+        cb(null, res);
     };
 
 }
